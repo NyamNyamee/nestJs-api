a6ef9861f332ecc7321344d9dc18931b8a5de8c6
inwoo <jopelee2@gmail.com>
2022-06-16 18:02:34 +0900

    movieService unitesting...update remains

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,4 +1,6 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import exp from 'constants';
 import { MoviesService } from './movies.service';
 
 describe('MoviesService', () => {
@@ -14,5 +16,69 @@ describe('MoviesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAll', () => {
+    it('shoud return an array', () => {
+      expect(service.getAll()).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    it('should return a move', () => {
+      service.create({
+        title: 'Test Movie',
+        year: 2022,
+        genres: ['Action'],
+      });
+
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.title).toEqual('Test Movie');
+    });
+
+    it('should throw NFE', () => {
+      try {
+        service.getOne(2);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe('delete', () => {
+    it('deletes a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        year: 2022,
+        genres: ['Action'],
+      });
+
+      const beforeDelete = service.getAll();
+      service.delete(1);
+      const afterDelete = service.getAll();
+      expect(afterDelete.length).toBeLessThan(beforeDelete.length);
+    });
+
+    it('shoud throw NFE', () => {
+      try {
+        service.delete(1);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe('create', () => {
+    it('creates a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        year: 2022,
+        genres: ['Action'],
+      });
+
+      expect(service.getOne(1)).toBeDefined();
+      expect(service.getAll()).toContain(service.getOne(1));
+    });
   });
 });
